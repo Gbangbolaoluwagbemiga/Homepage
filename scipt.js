e0f0bf799ce72045fa4c1a4a08f1f60e71b0b799
Gbangbola oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2022-06-17 08:41:15 -0700

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,8 @@
+// "use script";
+// const newUser = document.querySelector(".newuser");
+// const detail = document.querySelector(".detail");
+// const hide = document.querySelector(".hidden");
+// newUser.addEventListener("click", function () {
+//   detail.classList.add("hidden");
+//   hide.classList.remove("hidden");
+// });
